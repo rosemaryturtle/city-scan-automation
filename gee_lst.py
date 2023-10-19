c8ddb95f118262c685312bc03fcfe28101e019ed
Rui <rosemary.g.s@hotmail.com>
2023-10-19 18:15:11 -0400

    Add GEE LST processing

--- gee_lst.py
+++ gee_lst.py
@@ -8,8 +8,6 @@ with open("menu.yml", 'r') as f:
 if menu['gee']:
     import ee
     import geopandas as gpd
-    import geemap as gm
-    from pathlib import Path
 
     # SET UP #########################################
     # load city inputs files, to be updated for each city scan
@@ -18,40 +16,46 @@ if menu['gee']:
 
     city_name_l = city_inputs['city_name'].replace(' ', '_').lower()
 
+    # load global inputs
+    with open("global_inputs.yml", 'r') as f:
+        global_inputs = yaml.safe_load(f)
+
     # Initialize Earth Engine
     ee.Initialize()
 
     landsat = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
-    
-    # Set parameter
-    AOI = gm.shp_to_ee(city_inputs['AOI_path'])  # update this for each AOI
-    
-    # Filter for hottest months in the past X years
-    # years = range(city_inputs['first_year'],
-    #               city_inputs['last_year'] + 1)
 
-    # def filter_hot_month(year):
-    #     return ee.Filter.date(f"{str(year)}-{str(city_inputs['first_hot_month']).zfill(2)}-01", 
-    #                           f"{str(year)}-{str(int(city_inputs['last_hot_month']) + 1).zfill(2)}-01")
+    # Read AOI shapefile --------
+    aoi_file = gpd.read_file(city_inputs['AOI_path']).to_crs(epsg = 4326)
 
-    # rangefilter = ee.Filter.Or(*[filter_hot_month(year) for year in years])
+    # Convert shapefile to ee.Geometry ------------
+    jsonDict = eval(aoi_file['geometry'].to_json())
 
-    years = ['2018', '2019', '2020', '2021', '2022']
-    month0 = '06'  # first hottest month  # update for each country
-    month1 = '10'  # end of hottest month (note that this is exclusive)  # update for each country
+    if len(jsonDict['features']) > 1:
+        print('Need to convert polygons into a multipolygon')
+        print('or do something else, like creating individual raster for each polygon and then merge')
+        exit()
+    AOI = ee.Geometry.Polygon(jsonDict['features'][0]['geometry']['coordinates'])
 
-    def filter_hot_month(index):
-        return ee.Filter.date(years[index] + '-' + month0 + '-01', years[index] + '-' + month1 + '-01')
 
-    range0 = filter_hot_month(0)
-    range1 = filter_hot_month(1)
-    range2 = filter_hot_month(2)
-    range3 = filter_hot_month(3)
-    range4 = filter_hot_month(4)
+    # GEE PARAMETERS ################################
+    # Date filter -----------------
+    if city_inputs["last_hot_month"] > city_inputs["first_hot_month"]:
+        range_list = [ee.Filter.date(f'{year}-{city_inputs["first_hot_month"]}-01',
+                                     f'{year}-{city_inputs["last_hot_month"]+1}-01') for year in range(city_inputs['first_year'], 
+                                                                                                       city_inputs['last_year'] + 1)]
+    elif city_inputs["last_hot_month"] < city_inputs["first_hot_month"]:
+        range_list0 = [ee.Filter.date(f'{year}-{city_inputs["first_hot_month"]}-01',
+                                      f'{year+1}-01-01') for year in range(city_inputs['first_year'],
+                                                                           city_inputs['last_year'] + 1)]
+        range_list1 = [ee.Filter.date(f'{year}-01-01',
+                                      f'{year}-{city_inputs["last_hot_month"]+1}-01') for year in range(city_inputs['first_year'],
+                                                                                                        city_inputs['last_year'] + 1)]
+        range_list = range_list0 + range_list1
 
-    rangefilter = ee.Filter.Or(range0, range1, range2, range3, range4)
+    rangefilter = ee.Filter.Or(range_list)
 
-    # Define a function to scale the data and mask unwanted pixels
+    # Cloud mask function ----------------
     def maskL457sr(image):
         # Bit 0 - Fill
         # Bit 1 - Dilated Cloud
@@ -60,33 +64,21 @@ if menu['gee']:
         # Bit 4 - Cloud Shadow
         qaMask = image.select('QA_PIXEL').bitwiseAnd(int('11111', 2)).eq(0)
         saturationMask = image.select('QA_RADSAT').eq(0)
-
         # Apply the scaling factors to the appropriate bands.
         thermalBand = image.select('ST_B10').multiply(0.00341802).add(149.0)
-
         # Replace the original bands with the scaled ones and apply the masks.
         return image.addBands(thermalBand, None, True).updateMask(qaMask).updateMask(saturationMask)
 
-    # Apply filter and mask
-    collectionSummer = landsat \
-        .filter(rangefilter) \
-        .filterBounds(AOI) \
-        .map(maskL457sr) \
-        .select('ST_B10') \
-        .mean() \
-        .add(-273.15) \
-        .clip(AOI)
-    
-    # Export
-    task = ee.batch.Export.image.toDrive(**{
+
+    # GEE PROCESSING ###############################
+    collectionSummer = landsat.filter(rangefilter).filterBounds(AOI).map(maskL457sr).select('ST_B10').mean().add(-273.15).clip(AOI)
+    task = ee.batch.Export.image.toCloudStorage(**{
         'image': collectionSummer,
-        'crs': 'EPSG:4326',
         'description': f"{city_name_l}_summer",
-        'folder': city_inputs['country_name'],
-        'region': AOI.geometry(),
-        'skipEmptyTiles': True,
+        'bucket': global_inputs['cloud_bucket'],
+        # 'folder': city_inputs['country_name'],
+        'region': AOI,
         'scale': 30,
         'maxPixels': 1e9
     })
-
     task.start()
