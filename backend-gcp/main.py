dbae12d0ccda0ac97f9b77c85e3f51cf801a7787
Rui <rosemary.g.s@hotmail.com>
2024-09-03 18:57:19 -0400

    Create parallel tasks for cloud run job

--- backend-gcp/main.py
+++ backend-gcp/main.py
@@ -58,153 +58,155 @@ with open("global_inputs.yml", 'r') as f:
 ########################################################
 # RUN COMPONENTS #######################################
 ########################################################
+task_index = int(os.getenv('CLOUD_RUN_TASK_INDEX'))
 
-if menu['accessibility']:  # processing time: 40m
-    import accessibility
-    accessibility.accessibility(aoi_file, city_inputs, local_output_dir, city_name_l, cloud_bucket, output_dir)
-
-if menu['burned_area']:  # processing time: 5h
-    import burned_area
-    burned_area.burned_area(aoi_file, global_inputs['burned_area_blob'], data_bucket, local_data_dir, local_output_dir, city_name_l, cloud_bucket, output_dir)
-
-if menu['demographics']:  # processing time: 19m
-    import raster_pro
-    import requests
-
-    local_demo_folder = f'{local_data_dir}/demographics'
-    os.makedirs(local_demo_folder, exist_ok=True)
-
-    wp_file_json = requests.get(f"https://www.worldpop.org/rest/data/age_structures/ascic_2020?iso3={city_inputs['country_iso3']}").json()
-    wp_file_list = wp_file_json['data'][0]['files']
-
-    raster_pro.download_raster(wp_file_list, local_demo_folder, data_bucket, data_bucket_dir='WorldPop age structures')
-
-    sexes = ['f', 'm']
-
-    with open(f'{local_output_dir}/{city_name_l}_demographics.csv', 'w') as f:
-        f.write('age_group,sex,population\n')
-
-        for i in [1] + list(range(0, 85, 5)):
-            for s in sexes:
-                out_image, out_meta = raster_pro.raster_mask_file(f'{local_demo_folder}/{city_inputs["country_iso3"].lower()}_{s}_{i}_2020_constrained.tif', features)
-                out_image[out_image == out_meta['nodata']] = 0
-
-                if i == 0:
-                    age_group_label = '0-1'
-                elif i == 1:
-                    age_group_label = '1-4'
-                elif i == 80:
-                    age_group_label = '80+'
-                else:
-                    age_group_label = f'{i}-{i+4}'
-                
-                f.write('%s,%s,%s\n' % (age_group_label, s, sum(sum(sum(out_image)))))
-    
-    utils.upload_blob(cloud_bucket, f'{local_output_dir}/{city_name_l}_demographics.csv', f'{output_dir}/{city_name_l}_demographics.csv')
-
-if menu['elevation']:  # processing time: 6m
-    import elevation
-    elevation.elevation(aoi_file, local_data_dir, data_bucket, city_name_l, local_output_dir, cloud_bucket, output_dir)
-
-if menu['flood_coastal'] or menu['flood_fluvial'] or menu['flood_pluvial']:  # processing time (2020 + 1 SSP): 13m
-    import fathom
-    
-    with open('fathom_aws_credentials.yml', 'r') as f:
-        fathom_aws_credentials = yaml.safe_load(f)
-        aws_access_key_id = fathom_aws_credentials['aws_access_key_id']
-        aws_secret_access_key = fathom_aws_credentials['aws_secret_access_key']
-    aws_bucket = global_inputs['fathom_aws_bucket']
-    
-    fathom.process_fathom(aoi_file, city_name_l, local_data_dir, city_inputs, menu, aws_access_key_id, aws_secret_access_key, aws_bucket, data_bucket, 'Fathom', local_output_dir, cloud_bucket, output_dir)
-
-if menu['forest']:  # processing time (all GEE): 1m
-    import gee_fun
-    gee_fun.gee_forest(city_name_l, aoi_file, cloud_bucket, output_dir)
-
-if menu['fwi']:  # processing time: 5m
-    import fwi
-    fwi.fwi(aoi_file, local_data_dir, data_bucket, city_inputs['fwi_first_year'], city_inputs['fwi_last_year'], global_inputs['fwi_blob'], local_output_dir, city_name_l, cloud_bucket, output_dir)
-
-if menu['green']:
-    import gee_fun
-    gee_fun.gee_ndxi(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, index_type = 'ndvi')
-
-if menu['landcover']:
-    import gee_fun
-    gee_fun.gee_landcover(city_name_l, aoi_file, local_output_dir, cloud_bucket, output_dir)
-    utils.upload_blob(cloud_bucket, f"{local_output_dir}/{city_name_l}_lc.csv", f'{output_dir}/{city_name_l}_lc.csv')
-
-if menu['landcover_burn']:  # processing time: 40s
-    import landcover_burnability
-    landcover_burnability.landcover_burn(city_name_l, aoi_file, data_bucket, global_inputs['lc_burn_blob'], local_output_dir, cloud_bucket, output_dir)
-
-if menu['lst_summer']:
-    import gee_fun
-    gee_fun.gee_lst(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, season = 'summer')
-
-if menu['lst_winter']:
-    import gee_fun
-    gee_fun.gee_lst(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, season = 'winter')
-
-if menu['ndmi']:
-    import gee_fun
-    gee_fun.gee_ndxi(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, index_type = 'ndmi')
-
-if menu['nightlight']:
-    import gee_fun
-    gee_fun.gee_nightlight(city_name_l, aoi_file, cloud_bucket, output_dir)
-
-if menu['population']:  # processing time: 20s
-    import raster_pro
-    import requests
-    import rasterio
-
-    local_pop_folder = f'{local_data_dir}/pop'
-    os.makedirs(local_pop_folder, exist_ok=True)
-
-    wp_file_json = requests.get(f"https://hub.worldpop.org/rest/data/pop/cic2020_100m?iso3={city_inputs['country_iso3']}").json()
-    wp_file_list = wp_file_json['data'][0]['files']
-
-    downloaded_list = raster_pro.download_raster(wp_file_list, local_pop_folder, data_bucket, data_bucket_dir='WorldPop')
-    raster_pro.mosaic_raster(downloaded_list, local_pop_folder, f'{city_name_l}_population.tif')
-    out_image, out_meta = raster_pro.raster_mask_file(f'{local_pop_folder}/{city_name_l}_population.tif', features)
-    with rasterio.open(f'{local_output_dir}/{city_name_l}_population.tif', "w", **out_meta) as dest:
-        dest.write(out_image)
-    utils.upload_blob(cloud_bucket, f'{local_output_dir}/{city_name_l}_population.tif', f'{output_dir}/{city_name_l}_population.tif')
-
-if menu['rwi']:  # processing time: 50s
-    import rwi
-    rwi.rwi(global_inputs['rwi_blob'], city_inputs['country_iso3'], data_bucket, local_data_dir, aoi_file, local_output_dir, city_name_l, cloud_bucket, output_dir)
-
-if menu['slope']:  # processing time: 20s
-    import raster_pro
-    raster_pro.slope(aoi_file, f'{local_output_dir}/{city_name_l}_elevation.tif', cloud_bucket, output_dir, city_name_l, local_output_dir)
-    raster_pro.get_raster_histogram(f'{local_output_dir}/{city_name_l}_slope.tif', [0, 2, 5, 10, 20, 90], f'{local_output_dir}/{city_name_l}_slope.csv')
-    utils.upload_blob(cloud_bucket, f'{local_output_dir}/{city_name_l}_slope.csv', f'{output_dir}/{city_name_l}_slope.csv')
-
-if menu['wsf']:  # processing time: 30s
-    import wsf
-    wsf.wsf(aoi_file, local_data_dir, data_bucket, city_name_l, local_output_dir, cloud_bucket, output_dir)
-
-for i in ['air', 'landslide', 'liquefaction', 'solar']:  # processing time: 1m
-    if menu[i]:
-        import rasterio
+if task_index == 0:
+    if menu['road_network']:  # processing time: 12h
+        import road_network
+        road_network.road_network(city_name_l, aoi_file, local_output_dir, cloud_bucket, output_dir)
+elif task_index == 1:
+    if menu['accessibility']:  # processing time: 40m
+        import accessibility
+        accessibility.accessibility(aoi_file, city_inputs, local_output_dir, city_name_l, cloud_bucket, output_dir)
+elif task_index == 2:
+    if menu['burned_area']:  # processing time: 5h
+        import burned_area
+        burned_area.burned_area(aoi_file, global_inputs['burned_area_blob'], data_bucket, local_data_dir, local_output_dir, city_name_l, cloud_bucket, output_dir)
+elif task_index == 3:
+    if menu['demographics']:  # processing time: 19m
         import raster_pro
-        import gc
+        import requests
 
-        raster_bytes = utils.read_blob_to_memory(data_bucket, global_inputs[f'{i}_blob'])
-        out_image, out_meta = raster_pro.raster_mask_bytes(raster_bytes, features)
-        with rasterio.open(f'{local_output_dir}/{city_name_l}_{i}.tif', "w", **out_meta) as dest:
+        local_demo_folder = f'{local_data_dir}/demographics'
+        os.makedirs(local_demo_folder, exist_ok=True)
+
+        wp_file_json = requests.get(f"https://www.worldpop.org/rest/data/age_structures/ascic_2020?iso3={city_inputs['country_iso3']}").json()
+        wp_file_list = wp_file_json['data'][0]['files']
+
+        raster_pro.download_raster(wp_file_list, local_demo_folder, data_bucket, data_bucket_dir='WorldPop age structures')
+
+        sexes = ['f', 'm']
+
+        with open(f'{local_output_dir}/{city_name_l}_demographics.csv', 'w') as f:
+            f.write('age_group,sex,population\n')
+
+            for i in [1] + list(range(0, 85, 5)):
+                for s in sexes:
+                    out_image, out_meta = raster_pro.raster_mask_file(f'{local_demo_folder}/{city_inputs["country_iso3"].lower()}_{s}_{i}_2020_constrained.tif', features)
+                    out_image[out_image == out_meta['nodata']] = 0
+
+                    if i == 0:
+                        age_group_label = '0-1'
+                    elif i == 1:
+                        age_group_label = '1-4'
+                    elif i == 80:
+                        age_group_label = '80+'
+                    else:
+                        age_group_label = f'{i}-{i+4}'
+                    
+                    f.write('%s,%s,%s\n' % (age_group_label, s, sum(sum(sum(out_image)))))
+        
+        utils.upload_blob(cloud_bucket, f'{local_output_dir}/{city_name_l}_demographics.csv', f'{output_dir}/{city_name_l}_demographics.csv')
+elif task_index == 4:
+    if menu['population']:  # processing time: 20s
+        import raster_pro
+        import requests
+        import rasterio
+
+        local_pop_folder = f'{local_data_dir}/pop'
+        os.makedirs(local_pop_folder, exist_ok=True)
+
+        wp_file_json = requests.get(f"https://hub.worldpop.org/rest/data/pop/cic2020_100m?iso3={city_inputs['country_iso3']}").json()
+        wp_file_list = wp_file_json['data'][0]['files']
+
+        downloaded_list = raster_pro.download_raster(wp_file_list, local_pop_folder, data_bucket, data_bucket_dir='WorldPop')
+        raster_pro.mosaic_raster(downloaded_list, local_pop_folder, f'{city_name_l}_population.tif')
+        out_image, out_meta = raster_pro.raster_mask_file(f'{local_pop_folder}/{city_name_l}_population.tif', features)
+        with rasterio.open(f'{local_output_dir}/{city_name_l}_population.tif', "w", **out_meta) as dest:
             dest.write(out_image)
+        utils.upload_blob(cloud_bucket, f'{local_output_dir}/{city_name_l}_population.tif', f'{output_dir}/{city_name_l}_population.tif')
+elif task_index == 5:
+    if menu['wsf']:  # processing time: 30s
+        import wsf
+        wsf.wsf(aoi_file, local_data_dir, data_bucket, city_name_l, local_output_dir, cloud_bucket, output_dir)
+elif task_index == 6:
+    if menu['elevation']:  # processing time: 6m
+        import elevation
+        elevation.elevation(aoi_file, local_data_dir, data_bucket, city_name_l, local_output_dir, cloud_bucket, output_dir)
 
-        utils.upload_blob(cloud_bucket, f"{local_output_dir}/{city_name_l}_{i}.tif", f"{output_dir}/{city_name_l}_{i}.tif")
+    if menu['slope']:  # processing time: 20s
+        import raster_pro
+        raster_pro.slope(aoi_file, f'{local_output_dir}/{city_name_l}_elevation.tif', cloud_bucket, output_dir, city_name_l, local_output_dir)
+        raster_pro.get_raster_histogram(f'{local_output_dir}/{city_name_l}_slope.tif', [0, 2, 5, 10, 20, 90], f'{local_output_dir}/{city_name_l}_slope.csv')
+        utils.upload_blob(cloud_bucket, f'{local_output_dir}/{city_name_l}_slope.csv', f'{output_dir}/{city_name_l}_slope.csv')
+elif task_index == 7:
+    if menu['flood_coastal'] or menu['flood_fluvial'] or menu['flood_pluvial']:  # processing time (2020 + 1 SSP): 13m
+        import fathom
+        
+        with open('fathom_aws_credentials.yml', 'r') as f:
+            fathom_aws_credentials = yaml.safe_load(f)
+            aws_access_key_id = fathom_aws_credentials['aws_access_key_id']
+            aws_secret_access_key = fathom_aws_credentials['aws_secret_access_key']
+        aws_bucket = global_inputs['fathom_aws_bucket']
+        
+        fathom.process_fathom(aoi_file, city_name_l, local_data_dir, city_inputs, menu, aws_access_key_id, aws_secret_access_key, aws_bucket, data_bucket, 'Fathom', local_output_dir, cloud_bucket, output_dir)
+elif task_index == 8:
+    if menu['fwi']:  # processing time: 5m
+        import fwi
+        fwi.fwi(aoi_file, local_data_dir, data_bucket, city_inputs['fwi_first_year'], city_inputs['fwi_last_year'], global_inputs['fwi_blob'], local_output_dir, city_name_l, cloud_bucket, output_dir)
+elif task_index == 9:
+    if menu['landcover_burn']:  # processing time: 40s
+        import landcover_burnability
+        landcover_burnability.landcover_burn(city_name_l, aoi_file, data_bucket, global_inputs['lc_burn_blob'], local_output_dir, cloud_bucket, output_dir)
+elif task_index == 10:
+    if menu['rwi']:  # processing time: 50s
+        import rwi
+        rwi.rwi(global_inputs['rwi_blob'], city_inputs['country_iso3'], data_bucket, local_data_dir, aoi_file, local_output_dir, city_name_l, cloud_bucket, output_dir)
+elif task_index == 11:
+    for i in ['air', 'landslide', 'liquefaction', 'solar']:  # processing time: 1m
+        if menu[i]:
+            import rasterio
+            import raster_pro
+            import gc
 
-        del raster_bytes
-        gc.collect()
+            raster_bytes = utils.read_blob_to_memory(data_bucket, global_inputs[f'{i}_blob'])
+            out_image, out_meta = raster_pro.raster_mask_bytes(raster_bytes, features)
+            with rasterio.open(f'{local_output_dir}/{city_name_l}_{i}.tif', "w", **out_meta) as dest:
+                dest.write(out_image)
 
-if menu['road_network']:  # processing time: 12h
-    import road_network
-    road_network.road_network(city_name_l, aoi_file, local_output_dir, cloud_bucket, output_dir)
+            utils.upload_blob(cloud_bucket, f"{local_output_dir}/{city_name_l}_{i}.tif", f"{output_dir}/{city_name_l}_{i}.tif")
+
+            del raster_bytes
+            gc.collect()
+elif task_index == 12:
+    if menu['forest']:  # processing time (all GEE): 1m
+        import gee_fun
+        gee_fun.gee_forest(city_name_l, aoi_file, cloud_bucket, output_dir)
+
+    if menu['green']:
+        import gee_fun
+        gee_fun.gee_ndxi(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, index_type = 'ndvi')
+
+    if menu['landcover']:
+        import gee_fun
+        gee_fun.gee_landcover(city_name_l, aoi_file, local_output_dir, cloud_bucket, output_dir)
+        utils.upload_blob(cloud_bucket, f"{local_output_dir}/{city_name_l}_lc.csv", f'{output_dir}/{city_name_l}_lc.csv')
+
+    if menu['lst_summer']:
+        import gee_fun
+        gee_fun.gee_lst(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, season = 'summer')
+
+    if menu['lst_winter']:
+        import gee_fun
+        gee_fun.gee_lst(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, season = 'winter')
+
+    if menu['ndmi']:
+        import gee_fun
+        gee_fun.gee_ndxi(city_name_l, aoi_file, local_output_dir, city_inputs['first_year'], city_inputs['last_year'], data_bucket, cloud_bucket, output_dir, index_type = 'ndmi')
+
+    if menu['nightlight']:
+        import gee_fun
+        gee_fun.gee_nightlight(city_name_l, aoi_file, cloud_bucket, output_dir)
 
 # TODO: create city folder and 3 subdirectories in crp-city-scan
 # circular process? Which one comes first: city directory or city_inputs.yml?
