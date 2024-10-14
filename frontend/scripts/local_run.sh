27389fdd1c7b9594700e6853762c6b11542baf4b
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2024-10-14 13:26:05 -0700

    Adjusting docker setup for LGCRRP

--- frontend/scripts/local_run.sh
+++ frontend/scripts/local_run.sh
@@ -7,8 +7,10 @@ echo "Mounted to $MNT_DIR"
 # to do 03-render-output because if the others don't exist, there's no content)
 mkdir -p $MNT_DIR/$CITY_DIR/01-user-input $MNT_DIR/$CITY_DIR/02-process-output $MNT_DIR/$CITY_DIR/03-render-output 
 
-# Instead of moving the files afterward, it might make sense to have a site folder that I build from
-quarto render index.qmd
-cp index.qmd $MNT_DIR/$CITY_DIR/03-render-output/index.qmd
-cp index.html $MNT_DIR/$CITY_DIR/03-render-output/index.html
-cp -r index_files $MNT_DIR/$CITY_DIR/03-render-output/index_files
+# # Instead of moving the files afterward, it might make sense to have a site folder that I build from
+# quarto render index.qmd
+# cp index.qmd $MNT_DIR/$CITY_DIR/03-render-output/index.qmd
+# cp index.html $MNT_DIR/$CITY_DIR/03-render-output/index.html
+# cp -r index_files $MNT_DIR/$CITY_DIR/03-render-output/index_files
+
+Rscript R/main.R
