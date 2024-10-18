d2fb2748407647dd150d16644ab8026cdc473532
Rui <rosemary.g.s@hotmail.com>
2024-10-18 11:28:38 -0400

    Change city directory setup for backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
 import os
+from shutil import copyfile
+import yaml
 
 # Set the environment variable in your code
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/Owner/OneDrive/Documents/Career/World Bank/CRP/other/google-cloud-city-scan-service-account-key.json"
 
-# TODO: create city folder and 3 subdirectories in crp-city-scan
+# load city inputs files, to be updated for each city scan
+with open("../mnt/city-directories/01-user-input/city_inputs.yml", 'r') as f:
+    city_inputs = yaml.safe_load(f)
+city_name_l = city_inputs['city_name'].replace(' ', '_').replace("'", '').lower()
+
+# make city directory
+os.makedirs(f'mnt/{city_name_l}/01-user-input/AOI', exist_ok=True)
+os.makedirs(f'mnt/{city_name_l}/02-process-output', exist_ok=True)
+os.makedirs(f'mnt/{city_name_l}/03-render-output', exist_ok=True)
+
+shp_suf = ['shp', 'shx', 'dbf', 'prj', 'sbn', 'sbx', 'fbn', 'fbx', 'ain', 'aih', 'ixs', 'mxs', 'atx', 'shp.xml', 'cpg', 'qix', 'idx']
+for suf in shp_suf:
+    orig_file = f'../mnt/city-directories/01-user-input/AOI/{city_inputs['AOI_shp_name']}.{suf}'
+    if os.path.exists(orig_file):
+        copyfile(orig_file, f'../mnt/city-directories/{city_name_l}/01-user-input/AOI/{city_name_l}.{suf}')
 
 # TODO: make sure AOI is a polygon of an appropriate size
 # TODO: if an AOI includes several polygons, make it a multipolygon
@@ -27,7 +43,6 @@ script_list = ["burned_area.py",
                'slope.py',
                'flood_stats.py'
                ]
-# script_list = ['landcover_burnability_gcp.py']
 
 for s in script_list:
     with open(s) as f:
