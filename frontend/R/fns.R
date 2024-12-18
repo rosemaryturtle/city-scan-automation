58014722ac1368c1c038681e7fcddc363fac28dc
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2024-12-17 17:43:23 -0800

    PDF changes fom Uzbek cities work - prepare_html() to clean up quarto render output - font sizes - pseudo 5 column

--- frontend/R/fns.R
+++ frontend/R/fns.R
@@ -945,6 +945,34 @@ Mode <- \(x, na.rm = F) {
   unique_values[which.max(tabulate(match(x, unique_values)))]
 }
 
+prepare_html <- \(in_file, out_file, css_file) {
+  library(rvest)
+  library(xml2)
+  pdf <- read_html(in_file)
+  # browser()
+  stylesheet_nodes <- html_elements(pdf, "link[rel=stylesheet]")
+  xml_attr(stylesheet_nodes[1], "href") <- css_file
+  xml2::xml_remove(stylesheet_nodes[-1])
+  setup_node <- html_elements(pdf, ".setup")
+  xml2::xml_remove(setup_node)
+  # Do I want to remove all div.cell? NO
+  # xml2::xml_remove(html_nodes(pdf, ".cell"))
+  ojs_script_node <- html_element(pdf, "script[src='index_files/libs/quarto-ojs/quarto-ojs-runtime.js']")
+  xml2::xml_remove(ojs_script_node)
+  module_nodes <- html_elements(pdf, "script[type=module]")
+  xml2::xml_remove(module_nodes)
+  ojs_module_node <- html_element(pdf, "script[type=ojs-module-contents]")
+  xml2::xml_remove(ojs_module_node)
+  js_nodes <- html_elements(pdf, "script[type='text/javascript']")
+  xml2::xml_remove(js_nodes)
+  json_node <- html_elements(pdf, "script[type='application/json']")
+  xml2::xml_remove(json_node)
+  nav_node <- html_element(pdf, ".navigation")
+  xml2::xml_remove(nav_node)
+  xml2::xml_remove(html_nodes(pdf, "script#quarto-html-after-body"))
+  write_html(pdf, out_file)
+}
+
 rotate_ccw <- \(x) t(x)[ncol(x):1,]
 
 density_rast <- \(points, n = 100) {
