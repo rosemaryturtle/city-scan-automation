928514ad9444165c59bc04f84262bf507ab2debd
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2025-02-05 10:41:41 -0800

    Use run.sh for both Google Cloud and local runs; can now download from GCS to local. NEW DOCKER RUN COMMAND REQUIRED. See run.sh. Also reordered Dockerfile and include vivliostyle in container.

--- frontend/scripts/entrypoint.sh
+++ frontend/scripts/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-echo $CITY_DIR >> city-dir.txt
+# Formerly this was used to write $CITY_DIR to city-dir.txt but we have since
+# realized this is unnecessary. Keeping for any future entrypoint needs.
 exec "$@"
