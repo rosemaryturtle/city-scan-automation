58b17e8a29869dc2047ced73b725922b0383aa3e
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2025-08-21 06:48:12 -0400

    Save local version of elevation

--- frontend/R/map-elevation.R
+++ frontend/R/map-elevation.R
@@ -1,10 +1,10 @@
 tryCatch_named("elevation", {
   elevation_breaks <- fuzzy_read(tabular_dir, "elevation.csv", read_csv, col_types = "cd")$Bin %>%
     str_extract_all("\\d+") %>% unlist() %>% unique() %>% as.numeric()
-  elevation_data <- fuzzy_read(spatial_dir, layer_params$elevation$fuzzy_string) %>%
+  elevation_data <- fuzzy_read(spatial_dir, layer_params$elevation_local$fuzzy_string) %>%
           # aggregate_if_too_fine(threshold = 1e6, fun = \(x) Mode(x, na.rm = T)) %>%
           vectorize_if_coarse(threshold = 1e6)
-  plots$elevation <- plot_static_layer(
-    data = elevation_data, yaml_key = "elevation", breaks = elevation_breaks,
+  plots$elevation_local <- plot_static_layer(
+    data = elevation_data, yaml_key = "elevation_local", breaks = elevation_breaks,
     plot_aoi = T, plot_wards = !is.null(wards), zoom_adj = zoom_adjustment)
 })
