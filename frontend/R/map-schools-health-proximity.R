82cb03ea35c14acb43ec395bb93c4c2ff967c893
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2025-08-25 21:34:46 -0400

    handle spatvectorcollections for isochrones (for *_POI_unmodified files)

--- frontend/R/map-schools-health-proximity.R
+++ frontend/R/map-schools-health-proximity.R
@@ -18,25 +18,52 @@ tryCatch_named("health_proximity", {
 # Isochrones with unlabeled points
 c("public_space", "waste", "transportation", "sanitation", "electricity", "water", "communication") %>%
   walk(\(x) {
-    points <- fuzzy_read(spatial_dir, paste0(x, "_POI"))
+    features <- fuzzy_read(spatial_dir, paste0(x, "_POI_unmodified"), svc)
+    if (inherits(features, "SpatVectorCollection")) { 
+      geometries <- sapply(features, type_data)
+      points <- if ("points" %in% geometries) features[which(geometries == "points")] else vect()
+      polygons <- if ("polygons" %in% geometries) features[which(geometries == "polygons")] else vect()
+      lines <- if ("lines" %in% geometries) features[which(geometries == "lines")] else vect()
+      # For when polygons are too small to see, also label their centroids
+      if (nrow(polygons) > 0) points <- rbind(points, centroids(polygons))
+    } else {
+      points <- fuzzy_read(spatial_dir, paste0(x, "_POI\\."))
+      if (!inherits(points, "SpatVector")) return(NULL)
+      polygons <- vect()
+      lines <- vect()
+    }
     group <- layer_params[[paste0(x, "_points")]]$label
+    existence <- c(nrow(polygons) > 0, nrow(points) > 0, nrow(lines) > 0)
     p <- plots[[paste0(x, "_zones")]]
-    if (inherits(points, "SpatVector") & !is.null(p)) {
+    if (any(existence)) {
       points <- points[static_map_bounds] # Filter added for SEZ labels
+      polygons <- polygons[static_map_bounds]
+      lines <- lines[static_map_bounds]
       # Just use zones if nearest points are outside the mapping bounds
-      if (nrow(points) == 0) {
+      if (nrow(points) == 0 & nrow(polygons) == 0 & nrow(lines) == 0) {
         plots[[paste0(x, "_proximity")]] <<- p
         return(NULL)
       }
+      
+      geoms <- list(
+        geom_spatvector(data = polygons, aes(fill = group), color = NA, size = 1),
+        geom_spatvector(data = points, aes(color = group), size = 1),
+        geom_spatvector(data = lines, aes(color = group), size = 1)
+        )[existence]
+      scales <- list(
+        scale_fill_manual(values = layer_params[[paste0(x, "_points")]]$palette, name = "Feature"),
+        scale_color_manual(values = layer_params[[paste0(x, "_points")]]$palette, name = "Feature"),
+        scale_color_manual(values = layer_params[[paste0(x, "_points")]]$palette, name = "Feature"))[existence]
+
       # Points map
       plots[[paste0(x, "_points")]] <<- plot_static_layer(aoi_only = T, plot_aoi = T) +
-        geom_spatvector(data = points, aes(color = group), size = 1) +
-        scale_color_manual(values = layer_params[[paste0(x, "_points")]]$palette, name = "Feature") +
+        geoms + scales +
         coord_3857_bounds(static_map_bounds)
       # Points & isochrones map (ggpacket would let me reuse the code)
+      if (is.null(p)) return(NULL)
       plots[[paste0(x, "_proximity")]] <<- p +
-        geom_spatvector(data = points, aes(color = group), size = 1) +
-        scale_color_manual(values = layer_params[[paste0(x, "_points")]]$palette, name = "Feature") +
+        ggnewscale::new_scale_fill() +
+        geoms + scales +
         coord_3857_bounds(static_map_bounds)
     }
   })
