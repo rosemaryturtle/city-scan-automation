6abc53b83b06af72afe7e0858b036e398a72b387
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2025-08-27 13:17:33 -0400

    Don't add line breaks for null titles

--- frontend/R/fns.R
+++ frontend/R/fns.R
@@ -889,8 +889,9 @@ break_lines <- function(x, width = 20, newline = "<br>") {
 }
 
 format_title <- function(title, subtitle, width = 20) {
+  if ((is.null(title) || title == "") & (is.null(subtitle) || subtitle == "")) return(NULL)
   title_broken <- paste0(break_lines(title, width = width, newline = "<br>"), "<br>")
-  if (is.null(subtitle)) return(title_broken)
+  if (is.null(subtitle) || subtitle == "") return(title_broken)
   subtitle_broken <- break_lines(subtitle, width = width, newline = "<br>")
   formatted_title <- paste0(title_broken, "<br><em>", subtitle_broken, "</em><br>")
   return(formatted_title)
