ffbcb038383a31957a0c50161221fe757a5aa8aa
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2025-12-24 12:17:21 -0600

    tidying frontend directory

--- frontend/_pre-render.R
+++ frontend/_pre-render.R
@@ -6,7 +6,7 @@
 # This should maybe live in backend (as should much of this file)
 # Should this be a set of higher-level functions instead?
 # I also have begun drafting this into python: pre-render/write-population.py
-source("pre-render/write-population.R")
+source("R/write-population.R")
 
 # Generate plots
 # source('pre-render/plots.R')
